d0c361eb7f158e004af29c304ddaee496fc84aff
nivet <fun5xxx@gmail.com>
2019-12-30 18:04:42 +0800

    remove checkstyle

--- spring-cloud-kubernetes-examples/kubernetes-hello-world-example/src/main/java/org/springframework/cloud/kubernetes/examples/App.java
+++ spring-cloud-kubernetes-examples/kubernetes-hello-world-example/src/main/java/org/springframework/cloud/kubernetes/examples/App.java
@@ -26,8 +26,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class App {
-	private App(){
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
